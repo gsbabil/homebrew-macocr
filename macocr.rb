cd3cb18386963c90cb03cbd6722dd2d4bc32e0b2
Babil G. Sarwar <gsbabil@gmail.com>
2023-02-10 04:45:50 +1100

    Update MacOCR to v1.0.2

--- macocr.rb
+++ macocr.rb
@@ -1,8 +1,8 @@
 class Macocr < Formula
   desc "OCR command line utility for MacOS 10.15+ using the VNRecognizeTextRequest API"
   homepage "https://github.com/gsbabil/MacOCR"
-  url "https://github.com/gsbabil/MacOCR/archive/1.0.1.tar.gz"
-  sha256 "b12ed6035cf6f6519aabc0b77f944c8ac8196895af84bf5c4ff25c8d0b5dc97a"
+  url "https://github.com/gsbabil/MacOCR/archive/1.0.2.tar.gz"
+  sha256 "f9fbf0f54625262775655ecf2568add1044152c07a4b252c9ef0c43bd8f11180"
 
   def install
     system "swift", "build",
